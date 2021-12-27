4c581701a4816ff04dd43beab672d7bd62ea9e7c
Laha-hub <81339797 +Laha-hub@users.noreply.github.com>
2021-12-28 00:20:49 +0900

    #4 make navbar work

---
+++ script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+{
+    // navbar
+    const menu = document.querySelector('.menu');
+    const navbar = document.querySelector('.navbar');
+
+    menu.addEventListener('click', () => {
+        navbar.classList.toggle('change');
+        menu.classList.toggle('change');
+    });
+    // end of navbar
+}
